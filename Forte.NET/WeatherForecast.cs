49329c2d748d85c5d141e1b22b92e8ea7272b85c
Mcat12 <newtoncat12@yahoo.com>
2021-01-07 17:32:47 -0500

    Clean up unused imports, formatting, etc

--- Forte.NET/WeatherForecast.cs
+++ Forte.NET/WeatherForecast.cs
@@ -1,9 +1,7 @@
 using System;
 
-namespace Forte.NET
-{
-    public class WeatherForecast
-    {
+namespace Forte.NET {
+    public class WeatherForecast {
         public DateTime Date { get; set; }
 
         public int TemperatureC { get; set; }
