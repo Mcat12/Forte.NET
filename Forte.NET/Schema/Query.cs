e0a337b640126e601e6b850435937a5b344a3da7
Mcat12 <newtoncat12@yahoo.com>
2021-01-15 10:40:24 -0500

    Add basic pagination

--- Forte.NET/Schema/Query.cs
+++ Forte.NET/Schema/Query.cs
@@ -1,9 +1,9 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using Forte.NET.Database;
 using GraphQL;
 using GraphQL.Types;
+using GraphQL.Types.Relay.DataObjects;
 
 namespace Forte.NET.Schema {
     public class Query : ObjectGraphType {
@@ -15,20 +15,12 @@ namespace Forte.NET.Schema {
                 ),
                 resolve: context => SongById(context.ForteDbContext(), context.GetArgument<Guid>("id"))
             );
-            Field<ListGraphType<SongType>>(
-                "songs",
-                resolve: context => Songs(context.ForteDbContext())
-            );
             Field<NonNullGraphType<AlbumType>>(
                 "album",
                 arguments: new QueryArguments(
                     new QueryArgument<NonNullGraphType<IdGraphType>> { Name = "id" }
                 ),
                 resolve: context => AlbumById(context.ForteDbContext(), context.GetArgument<Guid>("id"))
-            );
-            Field<ListGraphType<AlbumType>>(
-                "albums",
-                resolve: context => Albums(context.ForteDbContext())
             );
             Field<NonNullGraphType<ArtistType>>(
                 "artist",
@@ -37,22 +29,50 @@ namespace Forte.NET.Schema {
                 ),
                 resolve: context => ArtistById(context.ForteDbContext(), context.GetArgument<Guid>("id"))
             );
-            Field<ListGraphType<ArtistType>>(
-                "artists",
-                resolve: context => Artists(context.ForteDbContext())
-            );
+
+            Connection<SongType>()
+                .Name("songs")
+                .Resolve(context => GetConnection(context, context.ForteDbContext().Songs));
+            Connection<AlbumType>()
+                .Name("albums")
+                .Resolve(context => GetConnection(context, context.ForteDbContext().Albums));
+            Connection<ArtistType>()
+                .Name("artists")
+                .Resolve(context => GetConnection(context, context.ForteDbContext().Artists));
+        }
+
+        private static Connection<T> GetConnection<T>(
+            IResolveFieldContext context,
+            IQueryable<T> dbSet
+        ) where T : class {
+            var first = context.GetArgument("first", 25);
+            var after = context.GetArgument<string?>("after");
+            var offset = after == null ? 0 : int.Parse(after);
+
+            var tableSize = dbSet.Count();
+            var hasNextPage = offset + first < tableSize;
+            var results = dbSet.Skip(offset).Take(first).ToList();
+            var edges = results
+                .Zip(Enumerable.Range(1, Int32.MaxValue))
+                .Select((item) => new Edge<T> {
+                    Cursor = (offset + item.Second).ToString(),
+                    Node = item.First
+                })
+                .ToList();
+
+            return new Connection<T> {
+                Edges = edges,
+                PageInfo = new PageInfo {
+                    HasNextPage = hasNextPage
+                },
+                TotalCount = tableSize
+            };
         }
 
         private static Song SongById(ForteDbContext context, Guid id) => context.Songs.Find(id);
 
-        private static List<Song> Songs(ForteDbContext context) => context.Songs.ToList();
-
         private static Album AlbumById(ForteDbContext context, Guid id) => context.Albums.Find(id);
 
-        private static List<Album> Albums(ForteDbContext context) => context.Albums.ToList();
-
         private static Artist ArtistById(ForteDbContext context, Guid id) => context.Artists.Find(id);
-
-        private static List<Artist> Artists(ForteDbContext context) => context.Artists.ToList();
     }
 }
